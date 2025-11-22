a9397ee140c03d4c8f87832ffed6d5e18a4a4c6f
Douwe de Vries <douwe.de.vries@crv4all.com>
2025-11-22 22:25:57 +0100

    fix: configure SignUp to redirect to / after signup

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -11,7 +11,7 @@ export default function SignUpPage() {
   return (
     <div className="flex min-h-screen items-center justify-center p-4">
       <div className="w-full max-w-md">
-        <SignUp />
+        <SignUp afterSignInUrl="/" afterSignUpUrl="/" />
       </div>
     </div>
   );
